3b9a99e6297a870a74345a519c2e40eb9322f676
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-04-02 12:27:18 -0700

    finished factorial

--- drills.js
+++ drills.js
@@ -99,3 +99,11 @@ let binaryRepresentation = num => {
   return '';
 };
 // console.log(binaryRepresentation(5));
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(5));
